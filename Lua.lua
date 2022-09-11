9e5723a1d820c28147cb529705e66e6966ac23f6
Notkonnerr32 <110717359+Notkonnerr32@users.noreply.github.com>
2022-09-10 19:51:44 -0500

    Add files via upload

---
+++ Lua.lua
@@ -0,0 +1,74 @@
+--credits to uhm idk me and my friend
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Player = game.Players.LocalPlayer 
+local Window = OrionLib:MakeWindow({Name = "Troll script", HidePremium = false, IntroText = "Loading", SaveConfig = true, ConfigFolder = "Config"})
+
+OrionLib:MakeNotification({
+	Name = "Welcome!",
+	Content = "Hello welcome to the script "..Player.Name..".",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+MainTab:AddButton({
+	Name = "Combat warriors script",
+	Callback = function()
+      		print("Loading script")
+            wait()
+            loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()
+
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Load admin scripts",
+	Callback = function()
+      		print("Loading")
+              loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
+              wait(1)
+              loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Load chat bypasser",
+	Callback = function()
+        loadstring(game:HttpGet("https://the-shed.xyz/roblox/scripts/ChatBypass", true))()
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "jenga script",
+	Callback = function()
+      		print("LOL")
+            wait()
+              loadstring(game:HttpGet("https://pastebin.com/raw/ndhWC6EM"))()
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Bedwars script",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "🚪 Apeirophobia Script",
+	Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/mVkMT64F"))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "Other",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
